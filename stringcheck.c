f179118be02867062cda0e2cbe7617ffb2f4480b
chai <chaitanyapaigude1216@gmail.com>
2025-07-29 18:16:46 +0530

    Add time/space complexity analysis and test cases to string solution,approach of CN sliding window

--- stringcheck.c
+++ stringcheck.c
@@ -1,4 +1,5 @@
 //getting max string length without duplications leet code problem 3 medium
+
 #include <stdio.h>
 #include <string.h>
 
@@ -24,3 +25,28 @@ int lengthOfLongestSubstring(char* s) {
 
     return maxLength;
 }
+
+int main() {
+    // Test cases
+    char test1[] = "abcabcbb";
+    char test2[] = "bbbbb";
+    char test3[] = "pwwkew";
+    char test4[] = "";
+    
+    printf("Test 1: \"%s\" -> Length: %d\n", test1, lengthOfLongestSubstring(test1));
+    printf("Test 2: \"%s\" -> Length: %d\n", test2, lengthOfLongestSubstring(test2));
+    printf("Test 3: \"%s\" -> Length: %d\n", test3, lengthOfLongestSubstring(test3));
+    printf("Test 4: \"%s\" -> Length: %d\n", test4, lengthOfLongestSubstring(test4));
+    
+    return 0;
+}
+
+/*
+ * Time Complexity: O(n) - where n is the length of the string
+ * Space Complexity: O(min(m,n)) - where m is the size of the charset (256 for ASCII)
+ * 
+ * Algorithm: Sliding Window Technique
+ * - Use two pointers (start and current position)
+ * - Keep track of last index of each character
+ * - When duplicate found, move start pointer to avoid repetition
+ */
