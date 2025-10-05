62c6cab0d59d5d1ae170e12ee3a6517c694d33c7
Anny <chaitanyapaigude1216@gmail.com>
2025-10-05 13:20:17 +0530

    learning stl

---
+++ CPP/stl.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //pairs/dictionary like
+    //remember pair is a data type that holds two values so you can use it to any other datastructure
+
+    pair<int,string> a;
+
+    cin >> a.first ;
+    cin >> a.second;
+    cout << "Your Id Password is saved" << endl;
+    cout << "Id: " << a.first << " Password: " << a.second << endl;
+
+    //Vectors is another datatstructure its like list in python but with numpy features such as getting an array but with fixed num and values u give it.
+
+    vector<int> v;
+}
