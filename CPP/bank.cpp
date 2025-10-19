186bdcb9e6e3416dcba54ae55ac58400d316ca8f
Anny <chaitanyapaigude1216@gmail.com>
2025-10-19 15:03:58 +0530

    OOPs implemented

--- CPP/bank.cpp
+++ CPP/bank.cpp
@@ -1,90 +1,139 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-long acc_num=0;
-long balancea=0;
-long balanceb=0;
-void create_account(){
-    cout<<"Account number :"<<acc_num+1<<endl;
-    cout<<"Name:";string name;cin>>name;cout<<endl;
-    cout<<"Email:";string ename;cin>>ename;cout<<endl;
-    cout<<"Addhar:";long adhar;cin>>adhar;cout<<endl;
-    cout<<"Mobile:";long mobile;cin>>mobile;cout<<endl;
-    cout<<"Account Pin:";int pin;cin>>pin;cout<<endl;
-    cout<<"Sucessfuly created account!";
+long acc_num_count = 0;
 
+class bank_customer
+{
+private:
+    long balance;
+    int pin;
+
+public:
+    long acc_num = 0;
+    string name;
+    long balancea = 0;
+    string ename;
+    long adhar;
+    long mobile;
+    int credit();
+    int debit();
+    void credit_to_party(int amt);
+    void create_account()
+    {
+        acc_num_count++;
+        acc_num = acc_num_count;
+        cout << "Account number :" << acc_num << endl;
+        cout << "Name:";
+        cin >> name;
+        cout << endl;
+        cout << "Email:";
+        
+        cin >> ename;
+        cout << endl;
+        cout << "Addhar:";
+        
+        cin >> adhar;
+        cout << endl;
+        cout << "Mobile:";
+        
+        cin >> mobile;
+        cout << endl;
+        cout << "Account Pin:";
+        cin >> pin;
+        cout << endl;
+        cout << "Sucessfuly created account!";
+    }
+  
+
+};
+
+int bank_customer :: credit () {
+            int amt;
+            cout<<"Enter amount:";
+            cin>>amt;
+            if(amt>0){balancea=balancea+amt;cout<<"Amount deposit Sucessful,New balance "<<balancea;}
+            else{cout<<"Invalid amount";}
+
+            return amt;
+            
 }
 
-void account_operations(){
-    int choice;
-    cout<<"1.Add credit 2.Debit cash 3.Check balance 4.Pay online 5.Back to menu";
-    cin>>choice;
-    switch(choice){
-        case 1:
-        int amt;
-        cout<<"Enter amount:";
-        cin>>amt;
-        if(amt>0){balancea=balancea+amt;cout<<"Amount deposit Sucessful,New balance "<<balancea;}
-        else{cout<<"Invalid amount";}
-        break;
+int bank_customer :: debit (){
+            int amtb;
+            int pin_check;
+            cout<<"Enter amount:";
 
-        case 2:
-        int amtb;
-        cout<<"Enter amount:";
-        cin>>amtb;
-        if(balancea>amtb){balancea=balancea-amtb;cout<<"Amount debited Sucessful,New balance "<<balancea;}
-        else{cout<<"Insufficient balance!";}
-        break;
+            cin>>amtb;
+            cout<<"Enter your pin:";
+            cin>>pin_check;
+            if (pin_check==pin)
+            {
+                if(balancea>amtb){balancea=balancea-amtb;cout<<"Amount debited Sucessful,New balance "<<balancea;}
+            else{cout<<"Insufficient balance!"; return 0;}
+            }
+            else{
+                cout<<"Invalid pin! Try again.";
+            }
+            return amtb;
+            
+            
+}
 
-        case 3:
-        cout<<"Current balance:"<<balancea;
-        break;
-
-        case 4:
-        int acno;
-        int amtc;
-        cout<<"Enter account no:";
-        cin>>acno;
-        cout<<"Enter amount:";
-        cin>>amtc;
-        if(balancea>amtc){balancea=balancea-amtc;cout<<"Amount debited Sucessful,New balance "<<balancea;balanceb=balanceb+amtc;}
-        else{cout<<"Insufficient balance!";}
-
-        case 5:
-                break;
-        default:
-                cout<<"Invalid choice. Please try again."<<endl;
-
-    }
+void bank_customer :: credit_to_party(int amt){
+    if(amt>0){balancea=balancea+amt;}
+            else{cout<<"Invalid amount";}
 
 }
 
 int main(void)
 {
-    int choice=1;
-    cout<<"Welcome to the Bank!"<<endl;
-   
-    while (choice!=0)
+
+    int choice = 1;
+    cout << "Welcome to the Bank!" << endl;
+    bank_customer arr[2];
+    for (int i = 0; i < 2; i++)
     {
-        cout<<"1.Create account 2.Account Operations "<<endl;
-        cin>>choice;
-        switch (choice)
-        {
-            case 1:
-                cout<<"Create Account Initiated"<<endl;
-                create_account();
-                break;
-            case 2:
-                cout<<"Account Operations."<<endl;
-                account_operations();
-                break;
-            
-            case 0:
-                cout<<"Thank you for using our services. Goodbye!"<<endl;
-                break;
-            default:
-                cout<<"Invalid choice. Please try again."<<endl;
-        }
+        arr[i].create_account();
     }
+    while (choice!=0){
+        int cd;
+        cout<<"Enter your bank account number:";
+        cin>>cd;
+        cout<<"1.Add credit 2.Debit cash 3.Check balance 4.Pay online 5.Back to menu";
+        cin>>choice;
+        switch(choice){
+            case 1:
+            arr[cd].credit();
+            break;
+
+            case 2:
+            arr[cd].debit();
+            break;
+
+            case 3:
+            
+            cout<<"Current balance:"<<arr[cd].balancea;
+            break;
+
+            case 4:
+            int payee_acno;
+            cout<<"Enter accno of payee:";
+            cin>>payee_acno;
+            
+            arr[payee_acno].credit_to_party(arr[cd].debit());
+            break;
+
+
+            case 5:
+                    break;
+            default:
+                    cout<<"Invalid choice. Please try again."<<endl;
+
+        }
+        cout<<"Enter 0 to exit";
+        cin>>choice;
+    }
+        
     
     return 0;
-} 
+}
